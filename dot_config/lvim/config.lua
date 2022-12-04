bae1032e0a68e7886d1993bc7cd61ade97a00199
FuruNov <clz14116@ict.nitech.ac.jp>
2022-12-04 20:25:33 +0900

    update

--- dot_config/lvim/config.lua
+++ dot_config/lvim/config.lua
@@ -18,7 +18,11 @@ lvim.use_icons = true
 -- keymappings [view all the defaults by pressing <leader>Lk]
 lvim.leader = "space"
 -- add your own keymapping
+lvim.keys.normal_mode["<ScrollWheelUp>"] = lvim.keys.normal_mode["<C-y>"]
+lvim.keys.normal_mode["<ScrollWheelDown>"] = lvim.keys.normal_mode["<C-e>"]
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
+lvim.keys.insert_mode["<C-s>"] = "<Esc>:w<cr>a"
+lvim.keys.visual_mode["<C-s>"] = "<Esc>:w<cr>v"
 lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
 lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
 lvim.keys.normal_mode["<Leader>r"] = ":LvimReload<CR>"
@@ -217,6 +221,25 @@ lvim.plugins = {
           let g:nnn#replace_netrw = 1
         ]])
       end,
+    },
+    {
+      "karb94/neoscroll.nvim",
+      event = "WinScrolled",
+      config = function()
+        require('neoscroll').setup({
+          -- All these keys will be mapped to their corresponding default scrolling animation
+          mappings = {'<C-u>', '<C-d>', '<C-b>', '<C-f>',
+                      '<C-y>', '<C-e>', 'zt', 'zz', 'zb'},
+          hide_cursor = true,          -- Hide cursor while scrolling
+          stop_eof = true,             -- Stop at <EOF> when scrolling downwards
+          use_local_scrolloff = false, -- Use the local scope of scrolloff instead of the global scope
+          respect_scrolloff = false,   -- Stop scrolling when the cursor reaches the scrolloff margin of the file
+          cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
+          easing_function = nil,        -- Default easing function
+          pre_hook = nil,              -- Function to run before the scrolling animation starts
+          post_hook = nil,              -- Function to run after the scrolling animation ends
+        })
+      end
     },
 }
 
