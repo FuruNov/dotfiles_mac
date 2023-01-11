a19f5be5621fca135ee3d2d28a97344dc41f8a24
FuruNov <clz14116@ict.nitech.ac.jp>
2023-01-11 17:28:43 +0900

    update

--- dot_julia/config/startup.jl
+++ dot_julia/config/startup.jl
@@ -1,6 +1,6 @@
 using OhMyREPL
 setargs!(ARGS, args...) = (empty!(ARGS); append!(ARGS, args))
 function run(script, args...)
-    setargs!(ARGS, args...)
+    setargs!(ARGS, script, args...)
     include(script)
 end
